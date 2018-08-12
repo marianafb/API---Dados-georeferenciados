eee81fe66f1b720fa3690afa733a3b7fb88a891d
marianafb <marianaboreli@gmail.com>
2018-08-12 09:31:32 -0300

    Update cepaberto_api_gh.R

--- cepaberto_api_gh.R
+++ cepaberto_api_gh.R
@@ -1,16 +1,18 @@
 
+#pacotes
 library(httr)
 library(rjson)
 
-token <- "seu token"
-cep_busca <- "cep desejado"
-query <- paste("http://www.cepaberto.com/api/v3/cep?cep=",cep_busca,sep="")
+token     <- "seu token"    #chave de identificação fornecida pelo site
+cep_busca <- "cep desejado" #cep que você deseja obter a latitude e longitude
+query     <- paste("http://www.cepaberto.com/api/v3/cep?cep=",cep_busca,sep="") #url 
 
-busca <- GET(url=query,add_headers('Authorization' = sprintf("Token %s", token)))
+busca <- GET(url=query,add_headers('Authorization' = sprintf("Token %s", token))) #requisição API
 
-if(is.null(busca$headers$status) == FALSE){
+#retirando as informações do site
+if(is.null(busca$headers$status) == FALSE){ 
   
-  dados<-fromJSON(content(busca,as="text"))
+  dados<-fromJSON(content(busca,as="text")) 
   
   base<-data.frame(cep = dados$cep, long = dados$longitude, lat = dados$latitude,
                    end = dados$logradouro, bairro = dados$bairro)
